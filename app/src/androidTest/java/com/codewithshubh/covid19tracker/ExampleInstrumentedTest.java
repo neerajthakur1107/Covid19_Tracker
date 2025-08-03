7f2db46feb205cae37842ea5c9023a9bbd12a10a
neerajthakur1107 <59472288+neerajthakur1107@users.noreply.github.com>
2025-08-03 16:53:29 +0900

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/codewithshubh/covid19tracker/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/codewithshubh/covid19tracker/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.codewithshubh.covid19tracker;
+package com.codeproject.covid19tracker;
 
 import android.content.Context;
 
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.codewithshubh.covid19tracker", appContext.getPackageName());
+        assertEquals("com.codeproject.covid19tracker", appContext.getPackageName());
     }
 }
